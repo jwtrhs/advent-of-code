a1b1c86e725909ed8b5aa4ee71a18b7355362241
Jonathon Waterhouse <jon@wtrhs.com>
2024-12-13 19:57:16 +1000

    Add 2024-11 - Part 2

--- 2024/11/main.py
+++ 2024/11/main.py
@@ -1,23 +1,48 @@
+import itertools
 import sys
+from typing import Iterable
 
-stones = [l for l in open(sys.argv[1]).readline().strip().split(" ")]
+Stone = str
 
-
-def blink(s: str):
-    return (
-        ["1"]
-        if s == "0"
-        else [str(int(s[: len(s) // 2])), str(int(s[len(s) // 2 :]))]
-        if len(s) % 2 == 0
-        else [str(int(s) * 2024)]
-    )
+stones: list[Stone] = [l for l in open(sys.argv[1]).readline().strip().split(" ")]
 
 
-def count_stones(*stones: str, n: int = 0) -> int:
+# Maps a tuple of (Stone, number of blinks) to a count of the number of stones
+# left after that number of blinks
+_cache: dict[tuple[Stone, int], int] = {}
+
+
+def blink_recursive(*stones: Stone, n: int = 1) -> int:
     if n == 0:
         return len(stones)
-    return sum(count_stones(*blink(stone), n=n - 1) for stone in stones)
+
+    count = 0
+    for s in stones:
+        # Check cache first
+        if (s, n) in _cache:
+            count += _cache[(s, n)]
+            continue
+
+        # Calculate the next set of stones
+        stones_ = (
+            ["1"]
+            if s == "0"
+            else [str(int(s[: len(s) // 2])), str(int(s[len(s) // 2 :]))]
+            if len(s) % 2 == 0
+            else [str(int(s) * 2024)]
+        )
+        # Recursively call this function on the next set of stones and cache the result
+        count_ = blink_recursive(*stones_, n=n - 1)
+        _cache[(s, n)] = count_
+        count += count_
+    return count
 
 
-part1 = count_stones(*stones, n=25)
+part1 = blink_recursive(*stones, n=25)
 print(f"Part 1: {part1}")
+assert part1 == 202019
+
+
+part2 = blink_recursive(*stones, n=75)
+print(f"Part 2: {part2}")
+assert part2 == 239321955280205
