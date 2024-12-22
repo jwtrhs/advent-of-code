2c82e5339cfc6b774d802fe1994ed87653e67fa2
Jonathon Waterhouse <jon@wtrhs.com>
2024-12-22 21:00:15 +1000

    Add 2024-21 - Part 2

--- 2024/21/main.py
+++ 2024/21/main.py
@@ -1,4 +1,5 @@
 import functools
+import itertools
 import sys
 import typing
 
@@ -44,7 +45,7 @@ def is_valid(map_: Map, start: Position, moves: list[str]) -> bool:
     return True
 
 
-def moves(map_: Map, items: typing.Iterable[str]) -> list[str]:
+def all_moves(map_: Map, items: typing.Iterable[str]) -> list[str]:
     a = next(k for k, v in map_.items() if v == "A")
     start = a
     moves_ = []
@@ -66,10 +67,61 @@ def moves(map_: Map, items: typing.Iterable[str]) -> list[str]:
     return moves_
 
 
+def moves(map_: Map, start: Position, end: Position) -> list[str]:
+    assert start in map_
+    assert end in map_
+    x_dist = end[0] - start[0]
+    y_dist = end[1] - start[1]
+    left = ["<"] * -x_dist
+    right = [">"] * x_dist
+    up = ["^"] * -y_dist
+    down = ["v"] * y_dist
+    if is_valid(map_, start, left + up + down + right):
+        return left + up + down + right + ["A"]
+    else:
+        return right + down + up + left + ["A"]
+
+
+@functools.cache
+def directional(start: Position, end: Position, r: int) -> int:
+    moves_ = moves(DIRECTIONAL_KEYPAD_MAP, start, end)
+    if r == 1:
+        return len(moves_)
+    sum_ = sum(
+        directional(
+            next(p for p, v in DIRECTIONAL_KEYPAD_MAP.items() if v == a),
+            next(p for p, v in DIRECTIONAL_KEYPAD_MAP.items() if v == b),
+            r=r - 1,
+        )
+        for a, b in itertools.pairwise(["A"] + moves_)
+    )
+    return sum_
+
+
+def numeric(code: str, r: int) -> int:
+    sum_ = 0
+    for a, b in itertools.pairwise("A" + code):
+        start = next(p for p, v in NUMERIC_KEYPAD_MAP.items() if v == a)
+        end = next(p for p, v in NUMERIC_KEYPAD_MAP.items() if v == b)
+        moves_ = moves(NUMERIC_KEYPAD_MAP, start, end)
+        sum_ += sum(
+            directional(
+                next(p for p, v in DIRECTIONAL_KEYPAD_MAP.items() if v == a),
+                next(p for p, v in DIRECTIONAL_KEYPAD_MAP.items() if v == b),
+                r=r,
+            )
+            for a, b in itertools.pairwise(["A"] + moves_)
+        )
+    return sum_
+
+
 score = 0
 for code in CODES:
-    moves_ = moves(NUMERIC_KEYPAD_MAP, code)
+    moves_ = all_moves(NUMERIC_KEYPAD_MAP, code)
     for i in range(0, 2):
-        moves_ = moves(DIRECTIONAL_KEYPAD_MAP, moves_)
+        moves_ = all_moves(DIRECTIONAL_KEYPAD_MAP, moves_)
     score += len(moves_) * int(code[:-1])
 print(f"Part 1: {score}")
+
+part2 = sum(numeric(code, r=25) * int(code[:-1]) for code in CODES)
+print(f"Part 2: {part2}")
